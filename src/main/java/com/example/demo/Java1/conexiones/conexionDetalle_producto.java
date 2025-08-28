429137e094b24d002869f8e06d6d2de49090eb9f
Stiven <moralesstiven047@gmail.com>
2025-08-27 22:49:50 -0500

    correción de errores, TODO quedó Funcional

--- src/main/java/com/example/demo/Java1/conexiones/conexionDetalle_producto.java
+++ src/main/java/com/example/demo/Java1/conexiones/conexionDetalle_producto.java
@@ -58,11 +58,13 @@ public class conexionDetalle_producto {
 
     @PutMapping("/detalle_producto/{id_detalle_producto}")
     public void actualizarDetalle_producto(@PathVariable int id_detalle_producto, @RequestBody detalle_producto detalleProducto) {
-        String sql = "UPDATE detalle_producto SET talla = ?, color = ?, precio=? WHERE id_detalle_producto = ?";
+        String sql = "UPDATE detalle_producto SET talla = ?, color = ?, id_producto=?, id_categoria=?, precio=? WHERE id_detalle_producto = ?";
         jdbcTemplate.update(sql,
 
                 detalleProducto.getTalla(),
                 detalleProducto.getColor(),
+                detalleProducto.getId_producto(),
+                detalleProducto.getId_categoria(),
                 detalleProducto.getPrecio(),
 
                 id_detalle_producto);
